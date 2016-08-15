4cdcbc9facca81ca267e2737e1cf8862dca950a6
Bang Nguyen and Tim Jung <bang+tim@>
2016-08-15 11:59:53 -0700

    create update functionality within rental-tile component

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -23,12 +23,16 @@ export default Ember.Component.extend({
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  updateRentalForm: false,
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    update(rental, params) {
+      this.sendAction('update', rental, params);
     },
     delete(rental) {
       if (confirm('Are you sure you want to delete this rental?')) {
