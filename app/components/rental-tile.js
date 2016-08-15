d2832bcbfd1ec99f49223a4ee94b7de22a924e9c
Bang Nguyen and Tim Jung <bang+tim@>
2016-08-15 14:27:36 -0700

    add rental route where users can update and delete the specific rental

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -23,21 +23,12 @@ export default Ember.Component.extend({
 
 export default Ember.Component.extend({
   isImageShowing: false,
-  updateRentalForm: false,
   actions: {
     imageShow() {
       this.set('isImageShowing', true);
     },
     imageHide() {
       this.set('isImageShowing', false);
-    },
-    update(rental, params) {
-      this.sendAction('update', rental, params);
-    },
-    delete(rental) {
-      if (confirm('Are you sure you want to delete this rental?')) {
-        this.sendAction('destroyRental', rental);
-      }
     }
   }
 });
