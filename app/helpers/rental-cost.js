5c409f06e418e158e309aede7983077a53fd6a4c
TJ2001 <jung.m.tim@gmail.com>
2016-08-22 13:14:39 -0700

    cost is being saved in firebase properly

--- app/helpers/rental-cost.js
+++ app/helpers/rental-cost.js
@@ -4,7 +4,9 @@ import Ember from 'ember';
 export function rentalCost(params) {
   var rentalPrice = params[0].get('cost');
 
-  if (rentalPrice >= 1200){
+  if (rentalPrice >= 2000){
+    return '$$$$$';
+  } else if (rentalPrice >= 1200){
     return '$$$$';
   } else if (rentalPrice >= 1000){
     return '$$$';
